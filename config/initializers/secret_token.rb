8035f8000726dced40338af705b2930999eb487a
Stephen McGillen <smcgillen@hotmail.com>
2013-08-21 12:14:13 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = 'c94436d9ffb5bafa02553881c6ea5170ad9c6d05d2badacf5ebd7a683d30c7501c7b8d87b4ed66913eeb7538de68ee85194ff5cc4a7cb09daa6c7761c1bd388e'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Quiz::Application.routes.draw do
+   root :to => 'home#index'
+end
