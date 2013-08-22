0e81b3ba8d4fa66f6994a0b98545a4026f658655
rdrakey <ryan@ryandrake.com>
2013-08-22 15:00:04 +1000

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Quiz::Application.routes.draw do
 resources :questions, :exams, :home, :answers, :attempts
 
-resources :users
+resources :users, :except => [:new]
 
 
    root :to => 'home#index'
 
+   get '/register' => 'users#new'
    get '/login' => 'session#new'
    get '/login' => 'session#create'
    get '/logout' => 'session#destroy'
