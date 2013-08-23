405e57703a2ef9e1dd48c1969570056818ec222c
hui <hui@hui-AO725.(none)>
2013-08-23 13:52:42 +1000

    user list added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
 	def new
-	@user = User.new
+		@user = User.new
 	end
 
 	def index
-	@users = User.all
-	@balance = params[:balance]
+		@users = User.all
+		@balance = params[:balance]
 	end
 
 	def create
-	User.create(params[:user])
-	@users = User.order(:email)
-	respond_to do |format|
-	format.js { render :create }
-	end
-	end
+		User.create(params[:user])
+		@users = User.order(:email)
+		@user = User.new(params[:user])
+		# respond_to do |format|
+		# 	format.js { render :create }
+		respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
+		
