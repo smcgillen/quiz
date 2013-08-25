9085732cbd41e471def9000c9182e8a79b9a317a
Mike <jarrellmike@gmail.com>
2013-08-25 18:45:23 +1000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,16 @@
 Quiz::Application.routes.draw do
 
   resources :questions, :exams, :home, :answers, :attempts
-  resources :exams, :home, :answers, :attempts do
-    collection do
-        get '/chart/:exams', :action => 'chart'
-      end
-    end
+  # resources :home do
+  #   collection do
+  #       get '/chart/:exams', :action => 'chart'
+  #     end
+  #   end
 
-<<<<<<< HEAD
   resources :users
 
      root :to => 'home#index'
 
-     get '/login' => 'session#new'
-     get '/login' => 'session#create'
-     get '/logout' => 'session#destroy'
-=======
 	get '/login' => 'session#new'
 	post '/login' => 'session#create'
 	get '/logout' => 'session#destroy'
@@ -29,6 +24,5 @@ Quiz::Application.routes.draw do
   post '/search/' => 'home#search'
 
 
->>>>>>> 07dbd046a14bbbb9d577d8b843679c76c472de77
 end
 
